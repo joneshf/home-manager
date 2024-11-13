eb817c7eb5cac54703ff2491c1bfee50cd5e2348
joneshf <jones3.hardy@gmail.com>
2024-11-12 20:52:39 -0800

    Support more than one platform
    Suprisingly, I sometimes work on all four of the common architectures:
    - Darwin AMD64
    - Darwin ARM64
    - Linux AMD64
    - Linux ARM64
    
    So, all these packages need to be multi-platform. There's likely a
    better way to do this, but this works for now.

--- packages/git-spice/default.nix
+++ packages/git-spice/default.nix
@@ -12,14 +12,57 @@
 let
   original-binary-name = "gs";
 
+  platform =
+    if stdenv.hostPlatform.isCygwin then
+      if stdenv.hostPlatform.isAarch64 then
+        {
+          sha256 = "sha256-yXQUPRsMn6h9uX6qaGa910Vv7aXYuMm4Sflmt2q65/k=";
+          tarball-basename = "git-spice.Windows-arm64.tar.gz";
+        }
+      else if stdenv.hostPlatform.isx86_64 then
+        {
+          sha256 = "sha256-6m0ByoPiPtQVHVPnAaBEXh87agQ53zj8ZM6V0Xa0eB8=";
+          tarball-basename = "git-spice.Windows-x86_64.tar.gz";
+        }
+      else
+        throw "Unsupported architecture: ${stdenv.hostPlatform.config}"
+    else if stdenv.hostPlatform.isDarwin then
+      if stdenv.hostPlatform.isAarch64 then
+        {
+          sha256 = "sha256-MbO0JrR4cBS88fDLD+LfsOMHw+3d3uPZ8ILPTAfEG3s=";
+          tarball-basename = "git-spice.Darwin-arm64.tar.gz";
+        }
+      else if stdenv.hostPlatform.isx86_64 then
+        {
+          sha256 = "sha256-x/yC/Vv3gSkx93SBBZo2afxbVGVL2N4RPwRHKVerqOw=";
+          tarball-basename = "git-spice.Darwin-x86_64.tar.gz";
+        }
+      else
+        throw "Unsupported architecture: ${stdenv.hostPlatform.config}"
+    else if stdenv.hostPlatform.isLinux then
+      if stdenv.hostPlatform.isAarch64 then
+        {
+          sha256 = "sha256-RcVjhBssUMY5BMk/kEpDPxXv/P+7KuQ1l7ngJH2zwUE=";
+          tarball-basename = "git-spice.Linux-aarch64.tar.gz";
+        }
+      else if stdenv.hostPlatform.isx86_64 then
+        {
+          sha256 = "sha256-S03W98JOYAayjSBdjyCKtDHdQkyylTcBFMTaNUSYCCI=";
+          tarball-basename = "git-spice.Linux-x86_64.tar.gz";
+        }
+      else
+        throw "Unsupported architecture: ${stdenv.hostPlatform.config}"
+    else
+      throw "Unsupported operating system: ${stdenv.hostPlatform.config}";
+
   pname = "git-spice";
 
   src = fetchzip {
-    sha256 = "sha256-x/yC/Vv3gSkx93SBBZo2afxbVGVL2N4RPwRHKVerqOw=";
+    sha256 = platform.sha256;
 
     stripRoot = false;
 
-    url = "https://github.com/abhinav/git-spice/releases/download/v${version}/git-spice.Darwin-x86_64.tar.gz";
+    url = "https://github.com/abhinav/git-spice/releases/download/v${version}/${platform.tarball-basename}";
   };
 
   version = "0.8.1";
--- packages/restack/default.nix
+++ packages/restack/default.nix
@@ -3,12 +3,42 @@
 }:
 
 let
+  platform =
+    if stdenv.hostPlatform.isDarwin then
+      if stdenv.hostPlatform.isAarch64 then
+        {
+          sha256 = "sha256-rFnFjassYWANGOIDMrTjaygi8hMbY63ucSzIDrD7eaU=";
+          tarball-basename = "restack-darwin-arm64.tar.gz";
+        }
+      else if stdenv.hostPlatform.isx86_64 then
+        {
+          sha256 = "sha256-9kkPnTGPnye5PLTMm1lBMJ783lFqZ56B4kPyEbyoz64=";
+          tarball-basename = "restack-darwin-amd64.tar.gz";
+        }
+      else
+        throw "Unsupported architecture: ${stdenv.hostPlatform.config}"
+    else if stdenv.hostPlatform.isLinux then
+      if stdenv.hostPlatform.isAarch64 then
+        {
+          sha256 = "sha256-xvH61V82H3mAj8N3V8J2ZI+yl16byR6jEpwUjqWToBA=";
+          tarball-basename = "restack-linux-arm64.tar.gz";
+        }
+      else if stdenv.hostPlatform.isx86_64 then
+        {
+          sha256 = "sha256-J8+A/LsHGJ93S/PbbaYDjdSEKPOu5BvrdM5Pi9kakkQ=";
+          tarball-basename = "restack-linux-amd64.tar.gz";
+        }
+      else
+        throw "Unsupported architecture: ${stdenv.hostPlatform.config}"
+    else
+      throw "Unsupported operating system: ${stdenv.hostPlatform.config}";
+
   pname = "restack";
 
   src = fetchzip {
-    sha256 = "sha256:1bngm2y13wj3wa0rwrvaa7ggr7ih85crpk5l7jwjg7wg66fhyjgn";
+    sha256 = platform.sha256;
 
-    url = "https://github.com/abhinav/restack/releases/download/v${version}/restack-darwin-amd64.tar.gz";
+    url = "https://github.com/abhinav/restack/releases/download/v${version}/${platform.tarball-basename}";
   };
 
   version = "0.8.0";
